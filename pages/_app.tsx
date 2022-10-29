6fcdea4034d419f954d8e47ca822ef520b0e5c7b
stevendev97 <x.limsteven@gmail.com>
2022-10-28 23:52:48 -0400

    Added Navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import Navbar from './navbar'
 
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  )
 }
